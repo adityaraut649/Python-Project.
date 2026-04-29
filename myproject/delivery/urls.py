369ba83d5b7fa1cb16779ffd31add3852bbc7ca9
adityaraut649 <vraut3468@gmail.com>
2026-04-29 18:50:15 +0530

    Adding function

--- myproject/delivery/urls.py
+++ myproject/delivery/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.say_hello , name="say_hello"),   
     path('Open_signup', views.Open_signup , name = "Open_signup" ),
     path('Open_signin', views.Open_signin , name = "Open_signin" ),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name="signin")
 ]
--- myproject/delivery/views.py
+++ myproject/delivery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Customer
 # Create your views here.
 
 def say_hello(request):
@@ -15,12 +17,36 @@ def Open_signin(request):
 
 def signup(request):
     if request.method == 'POST':
-        if request.method == 'POST':
             username = request.POST.get('username')
             password = request.POST.get('password')
             email = request.POST.get('email')
             mobile = request.POST.get('mobile')
             address = request.POST.get('address')
-            return HttpResponse("Sign up successful!")
-    else:
-        return HttpResponse("Invalid Response !")
+            try:
+                Customer.objects.get(username = username)
+                return HttpResponse("Duplicate username!")
+            except:
+                Customer.objects.create(
+                username = username,
+                password = password,
+                email = email,
+                mobile = mobile,
+                address = address,
+            )
+    return render(request, 'signin.html')           
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+    try:
+        Customer.objects.get(username = username)
+        if username == 'admin':
+           return render(request, 'admin_home.html')
+        else:
+           return render(request, 'customer_home.html')
+           
+       
+    except Customer.DoesNotExist:
+        return render(request, 'fail.html')
